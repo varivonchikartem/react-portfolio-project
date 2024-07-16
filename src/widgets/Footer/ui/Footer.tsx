a8b67b6f28b78f4c86af0f8344552f362f4a7aad
Varivonchik Artem <varivonchikartem.business@gmail.com>
2024-07-16 23:51:05 +0300

    Create a responsive Footer adaptable to various devices

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -9,9 +9,27 @@ interface FooterProps {
 export const Footer: React.FC<FooterProps> = (props) => {
   const { className } = props;
 
-  const footerClasses = clx(styles.Footer, {
+  const footerClasses = clx(styles.Footer, "container", {
     [className!]: className,
   });
 
-  return <div className={footerClasses}>Footer</div>;
+  return (
+    <footer className={footerClasses}>
+      <nav className={styles.FooterWrapper}>
+        <a className={styles.link} href="#">
+          Varivonchik A.A
+        </a>
+        <div className={styles.socials}>
+          <p>Linkedin</p>
+          <p>Instagram</p>
+          <p>Twitter</p>
+          <p>Reading</p>
+        </div>
+        <div className={styles.email}>
+          <p>Let's build something together.</p>
+          <p>varivonchikartem.business@gmail.com</p>
+        </div>
+      </nav>
+    </footer>
+  );
 };
